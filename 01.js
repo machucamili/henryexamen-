db44b0274d59b4bc5fe6e4e1fa5f52cf7eeb0a2d
Milagros Machuca <103150129+machucamili@users.noreply.github.com>
2022-10-09 06:20:28 -0300

    .
    .

--- 01.js
+++ 01.js
@@ -11,8 +11,8 @@ function palabrasAFrase(array) {
   // palabrasAFrase(['Dobby', 'loves', 'socks!]) debe retornar 'Dobby loves socks!'
   // sin espacios ni antes ni después es decir así no => " Dobby loves socks! "
   // Tu código aca:
-  return(array.join(" "));
- 
+  
+    { return(array.join(" "))
 }
 
 // No modifiques nada debajo de esta linea //
